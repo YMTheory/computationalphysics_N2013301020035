415cc1ac2bde9ab9defa416343e8cf449aacefff
YMTheory <932899789@qq.com>
2016-03-28 23:58:21 +0800

    completion

--- ChapterOne/Activity.cpp
+++ ChapterOne/Activity.cpp
@@ -8,47 +8,76 @@
 using namespace std;
 int Activity(void)
 {
-    double N_U[100000];
-    double t[100000];
-    double real_N[100000];
-    double dt = 0.01;
-    double ex[100000];
-    double ey[100000];
-    int tau = 10;
-    N_U[0] = 1000;
+    double dt,endtime;
+
+    cout<<"Please print the value of dt"<<endl;
+    cin>>dt;
+
+    cout<<"Please print the value of endtime"<<endl;
+    cin>>endtime;
+
+
+    
+    //const n = endtime/dt;
+   // cout<<n<<endl;
+    double N_U[10000];
+    double t[10000];
+    double real_N[10000];
+    double ex[10000];
+    double ey[10000];
+    int tau;
+   // N_U[0] =;
     t[0] = 0;
-    real_N[0] = 1000;
+   // real_N[0] = 1000;
     ey[0] = 0;
-    int endtime = 50;
+    cout<<"Please print the value of tau"<<endl;
+    cin>>tau;
+    cout<<"Please print the value of N_U[0]"<<endl;
+    cin>>N_U[0];
+    real_N[0] = N_U[0];
+    //cout<<"Please print the value of real_N"<<endl;
     int i = 0;
     double tmp = 0;
-    for(int j=0;j<100000;j++){ex[j] = 0;}
-	while (t[i] < endtime && N_U[i]>0)
+    for(int j=0;j<10000;j++){ex[j] = 0;}
+    
+    while(t[i]<endtime)
     {
 	tmp = N_U[i] - N_U[i]*dt/tau;
 	cout<<N_U[i]<<" "<<real_N[i]<<" "<<ey[i]<<endl;
 	i++;
+        cout<<tmp<<endl;
 	N_U[i] = tmp;
 	t[i] = dt*(i);
 	real_N[i] = real_N[0]*exp(-t[i]/tau);
         ey[i] = real_N[i]-N_U[i];
     }
+
+
    TCanvas *ym = new TCanvas("Activity","Activity",1200,1000);
-   TGraph *g1=new TGraph(100000,t,N_U);
-   TGraph *g2=new TGraph(100000,t,real_N);
+   TGraph *g1=new TGraph(10000,t,N_U);
+   TGraph *g2=new TGraph(10000,t,real_N);
   // for(int j=0;j<100000;j++) 
    //{g1->SetPoint(j,time[j],N_U[j]);}
-   g1->SetMarkerSize(0.5);
+   g1->SetMarkerSize(0.3);
+   //g2->SetMarkerSize(0.5);
    g1->SetTitle("Radiactivity");
-   g1->SetMarkerStyle(8);
-   g1->Draw("apl");
-   g2->Draw("same");
-   g1->SetLineWidth(0.01);
-   g1->SetLineColor(4);
-   g2->SetLineWidth(0.01);
-   g2->SetLineColor(2);
    g1->GetXaxis()->SetTitle("time/s ");
-   g1->GetYaxis()->SetTitle("N_U");  
+  // g1->SetMarkerStyle(15);
+   g1->GetYaxis()->SetTitle("N_U"); 
+    
+   g2->SetMarkerColor(2);
+   g2->SetMarkerStyle(20);
+   g2->SetMarkerSize(0.3);
+   //g1->SetMarkerStyle(15);
+   //g1->SetLineWidth(0.01);
+  // g2->SetLineWidth(0.01);
+   g1->Draw("A*");
+   g2->Draw("Psame");
+   
+   TLegend *legend = new TLegend(.75,.80,.95,.95);
+   legend->AddEntry(g1,"Numerical result");
+   legend->AddEntry(g2,"Analytical result");
+   legend->Draw();
   	
     return 0;
     
--- ChapterOne/Dou-Activity.cpp
+++ ChapterOne/Dou-Activity.cpp
@@ -9,7 +9,7 @@ int Dou_Activity(void)
 {
     double NA_U[10000];
     double NB_U[10000];
-    double t[100000];
+    double t[10000];
     double dt = 0.05;
     int tau = 20;
     NA_U[0] = 100.;
@@ -29,25 +29,34 @@ int Dou_Activity(void)
 	t[i] = dt*(i);
     }
    TCanvas *ym = new TCanvas("c1","Dou-Radiactivity",200,10,600,400);
-   TGraph *g1=new TGraph(100000,t,NA_U);
- //  TGraph *g2=new TGraph(100000,t,NB_U);
-  // for(int j=0;j<100000;j++) 
-   //{g1->SetPoint(j,time[j],N_U[j]);}
+   TGraph *g1=new TGraph(10000,t,NA_U);
+   TGraph *g2=new TGraph(10000,t,NB_U);
+//   for(int j=0;j<100000;j++) 
+ // /{g1->SetPoint(j,time[j],N_U[j]);}
    
-   g1->SetLineColor(4);
-  // g1->Draw("AC*");
 
 //   g2->SetLineWidth(1);
-//   g2->SetMarkerStyle(21);
-//   g2->SetLineColor(2);
+   g2->SetMarkerStyle(10);
+   g2->SetMarkerColor(2);
+   g2->SetMarkerSize(0.1);
 
-   g1->SetMarkerSize(0.1);
+   g1->SetMarkerColor(1);
+   g1->SetMarkerSize(0.01);
    g1->SetTitle("Radiactivity");
    g1->SetMarkerStyle(8);
-   g1->Draw("apl");
-//   g2->Draw("apl");
    g1->GetXaxis()->SetTitle("time/s ");
+  // g1->GetXaxis()->SetRange(0,100);
    g1->GetYaxis()->SetTitle("N_U");  
+   g1->GetYaxis()->SetRange(0,110);
+  // g1->Draw("ap");
+   g1->Draw("A*");
+   g2->Draw("Psame");
+
+   TLegend *legend = new TLegend(.75,.80,.95,.95);
+   legend->AddEntry(g1,"A element");
+   legend->AddEntry(g2,"B element");
+   legend->Draw();
+
 	
     return 0;
     
